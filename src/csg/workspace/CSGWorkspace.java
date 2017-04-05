ff3c9a1696486afc5e78758631c3d1afcb81b737
dsli208 <ds.li@outlook.com>
2017-04-05 18:50:08 -0400

    Started constructing the layout for the first tab

--- src/csg/workspace/CSGWorkspace.java
+++ src/csg/workspace/CSGWorkspace.java
@@ -12,7 +12,15 @@ import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import properties_manager.PropertiesManager;
 import csg.CSGProp;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 /**
  *
  * @author dsli
@@ -31,7 +39,50 @@ public class CSGWorkspace extends AppWorkspaceComponent {
     Tab recitationDataTab;
     Tab scheduleDataTab;
     Tab projectDataTab;
-
+    
+    GridPane topCourseDataBox;
+    ComboBox subjectComboBox;
+    ComboBox numberComboBox;
+    ComboBox semesterComboBox;
+    
+    Label courseInfoLabel;
+    Label subjectLabel;
+    Label numberLabel;
+    Label semesterLabel;
+    Label yearLabel;
+    Label titleLabel;
+    Label instructorNameLabel;
+    Label instructorHomeLabel;
+    Label exportDirLabel;
+    TextField titleTextField;
+    TextField instructorNameTextField;
+    TextField instructorHomeTextField;
+    Label exportDirTextView;
+    Button changeExportDirButton;
+    
+    GridPane courseDataMiddleBox;
+    Label siteTemplateLabel;
+    Label siteTemplateDescriptionLabel;
+    Label templateDir;
+    Label sitePagesLabel;
+    TableView sitePages;
+    //checkBox declaration here?
+    
+    GridPane pageStyleDataBox;
+    Label pageStyleLabel;
+    Label bannerSchoolLabel;
+    Label leftFooterLabel;
+    Label rightFooterLabel;
+    Label stylesheetLabel;
+    ImageView bannerSchoolImage;
+    ImageView leftFooterImage;
+    ImageView rightFooterImage;
+    Button changeBannerSchoolImageButton;
+    Button changeLeftFooterImageButton;
+    Button changeRightFooterImageButton;
+    ComboBox stylesheetSelect;
+    Label stylesheetNote;
+    
     /**
      * The contstructor initializes the user interface, except for
      * the full office hours grid, since it doesn't yet know what
@@ -47,6 +98,36 @@ public class CSGWorkspace extends AppWorkspaceComponent {
         //Create the tabs for the TabPane
         courseDataTab = new Tab();
         courseDataTab.setText(CSGProp.COURSE_DETAILS_TAB.toString());
+
+        VBox courseDataTabHBox = new VBox();
+        
+        //Stuff for the top VBox, Course Information
+        topCourseDataBox = new GridPane();
+        subjectComboBox = new ComboBox();
+        numberComboBox = new ComboBox();
+        semesterComboBox = new ComboBox();
+        
+        courseInfoLabel = new Label();
+        subjectLabel = new Label();
+        numberLabel = new Label();
+        semesterLabel = new Label();
+        yearLabel = new Label();
+        titleLabel = new Label();
+        instructorNameLabel = new Label();
+        instructorHomeLabel = new Label();
+        exportDirLabel = new Label();
+        
+        titleTextField = new TextField();
+        instructorNameTextField = new TextField();
+        instructorHomeTextField = new TextField();
+        exportDirTextView = new Label();
+        changeExportDirButton = new Button();
+        
+        //Middle box content, the Site Template
+        siteTemplateLabel = new Label();
+        
+        courseDataTab.setContent(courseDataTabHBox);
+        
         taDataTab = new Tab();
         taDataTab.setText(CSGProp.TA_DATA_TAB.toString());
         recitationDataTab = new Tab();
